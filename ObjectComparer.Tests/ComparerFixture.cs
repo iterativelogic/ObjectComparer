e72d06c0ad16d387b24f8283417f198f1403e0f6
Nerkar Pranav <pranav.nerkar@tieto.com>
2019-04-09 14:07:40 +0530

    Add project files.

---
+++ ObjectComparer.Tests/ComparerFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObjectComparer.Tests
+{
+    public class Person
+    {
+        public List<string> Childrens { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasIdentity { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public int FlatNo { get; set; }
+        public string ApartmentName { get; set; }
+        public bool IsOccupied { get; set; }
+        public Person BelongsTo { get; set; }
+    }
+
+    [TestClass]
+    public class ComparerFixture
+    {
+
+
+        [TestMethod]
+        public void ComparePrimitiveTypeObjects()
+        {
+            var circuit = new Person
+            {
+                Childrens = new List<string> { "Short", "Circuit" },
+                Id = 1,
+                Name = "Circuit",
+                HasIdentity = true,
+                Address = new Address
+                {
+                    ApartmentName = "Shanti Kunj",
+                    FlatNo = 123,
+                    IsOccupied = true
+                }
+            };
+
+            var munna = new Person
+            {
+                Childrens = new List<string> { "Circuit", "Short" },
+                Id = 1,
+                Name = "Circuit",
+                HasIdentity = true,
+                Address = new Address
+                {
+                    ApartmentName = "Shanti Kunj",
+                    FlatNo = 123,
+                    IsOccupied = true
+                }
+            };
+
+            circuit.Address.BelongsTo = munna;
+            munna.Address.BelongsTo = munna;
+
+            Assert.IsTrue(Comparer.AreSimilar(circuit, munna));
+        }
+
+       
+    }
+}
---
+++ ObjectComparer/Comparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectComparer
+{
+    public class Comparer
+    {
+        public static bool AreSimilar<T>(T first, T second )
+        {
+            var propertyState = new RefTypeState
+            {
+                FirstObject = first,
+                SecondObject = second
+            };
+
+            var result = propertyState.AreEqual();
+
+            return result;
+        }
+        
+    }
+}
